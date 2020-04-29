9a2599783402355e1c5e4d04ca389f9aa1fb73f9
amgillenwater <amgillenwater@gmail.com>
2020-04-28 20:17:34 -0400

    what is this

--- homework.js
+++ homework.js
@@ -3,14 +3,14 @@
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
 
-function remove(array, member){
-    for (let name of array){
-        if (name === member){
-            array.push(member)
-        }
-    }
-    return array;
-} 
+// function remove(array, member){
+//     for (let name of array){
+//         if (name === member){
+//             array.push(member)
+//         }
+//     }
+//     return array;
+// } 
 
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
@@ -30,12 +30,24 @@ function remove(array, member){
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(arrays) {
+    let final = 0;
+    for (let number of arrays){
+        final += number
+    }
+    return final
+}
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(arrays){
+    return arrays.reduce((a,b) => (a + b)) / arrays.length;
+}
+
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
